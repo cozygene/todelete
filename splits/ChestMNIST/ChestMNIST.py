3a35d2acad2d9b780db5978ed8eadc289f0eb295
berkindurmus <59856478+berkindurmus@users.noreply.github.com>
2024-10-04 17:26:10 -0700

    Delete splits directory

--- splits/ChestMNIST/ChestMNIST.py
+++
@@ -1,4 +0,0 @@
-from medmnist import ChestMNIST
-train_dataset = ChestMNIST(split="train", download=True)
-val_dataset = ChestMNIST(split="val", download=True)
-test_dataset = ChestMNIST(split="test", download=True)
